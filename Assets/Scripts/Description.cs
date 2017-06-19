171378f17f669e430f3ecfb55d3038f700351681
Francisco Javier García Gutiérrez <javiergarcia.tech@gmail.com>
2017-06-18 19:55:51 -0500

    GUI Sketch
    A sketch for the GUI

---
+++ Assets/Scripts/Description.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Description : MonoBehaviour {
+	
+	[SerializeField]
+	private string nameString;
+	[SerializeField]
+	private string descriptionString;
+
+	public string GetName(){
+		return nameString;
+	}
+	public string GetDescription(){
+		return descriptionString;
+	}
+}
---
+++ Assets/Scripts/Loader.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Loader : MonoBehaviour {
+
+	public GameObject loadBar;
+
+	private Transform loadBarTr;
+	private float screenWidth;
+	private float progress = 0;
+	private bool loadScene = false;
+	private bool start = false;
+
+
+	void Start () {
+		loadBarTr = loadBar.transform;
+		screenWidth=Screen.width;
+		loadBarTr.position = new Vector3 ((screenWidth*(progress-1)),0,0);
+	}
+		
+	void Update () {
+		if (start == false) {
+			start = true;
+			ChangeScene ("Menu");
+		}
+
+		if (loadScene == true) {
+			//Space for some animation here
+		}
+	}
+
+	public void ChangeScene(string scene){
+		StartCoroutine (LoadNewScene(scene));
+		loadScene = true;
+	}
+
+	IEnumerator LoadNewScene(string scene){
+		yield return new WaitForSeconds(3f);
+		AsyncOperation async = SceneManager.LoadSceneAsync(scene);
+		while (!async.isDone) {
+			
+			progress = async.progress;
+			loadBarTr.position = new Vector3 ((screenWidth*(progress-1)),0,0);
+
+			yield return null;
+		}
+	}
+}
